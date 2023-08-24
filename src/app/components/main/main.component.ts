f4c107087bfb1effb71131adb770d5facc04af15
magdaPulter <magda.pulter@gmail.com>
2023-08-24 20:14:45 +0200

    navbar added

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CountriesListComponent } from '../countries-list/countries-list.component';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, CountriesListComponent]
 })
 export class MainComponent {
 
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  standalone: true,
+  imports: [MatIconModule]
+})
+export class NavbarComponent {
+
+}
