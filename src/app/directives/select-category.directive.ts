3964cb37784cb46a8b8a0840ddbe0ee9044ab4bb
magdaPulter <magda.pulter@gmail.com>
2023-08-30 10:39:34 +0200

    select directive create

---
+++ src/app/directives/select-category.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appSelectCategory]',
+  standalone: true
+})
+export class SelectCategoryDirective {
+
+  constructor() { }
+
+}
