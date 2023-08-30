5c5184e42d15c2b4951aa2ba37db53ab78f7e3e8
magdaPulter <magda.pulter@gmail.com>
2023-08-30 09:47:40 +0200

    population pipe added

--- src/app/components/country-item/country-item.component.ts
+++ src/app/components/country-item/country-item.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card'
 import { CountryModel } from 'src/app/models/country.model';
+import { PopulationPipe } from 'src/app/pipes/population.pipe';
 
 @Component({
   selector: 'app-country-item',
   templateUrl: './country-item.component.html',
   styleUrls: ['./country-item.component.css'], 
   standalone: true,
-  imports: [CommonModule, MatCardModule]
+  imports: [CommonModule, MatCardModule, PopulationPipe]
 })
 export class CountryItemComponent {
   @Input() country!: CountryModel
