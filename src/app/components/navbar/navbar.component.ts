03c71754393bc28d11f4121b3f472327e3add844
magdaPulter <magda.pulter@gmail.com>
2023-10-11 11:13:38 +0200

    back to home page added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
   standalone: true,
-  imports: [MatIconModule]
+  imports: [MatIconModule, RouterLink]
 })
 export class NavbarComponent {
 
